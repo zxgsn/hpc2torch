49efe7f9e0f84c0f4c0830eb16548c95e83c8efb
xgqdut2016 <kenan_gewei@163.com>
2024-12-10 16:26:28 +0800

    mlu rope

---
+++ src/rotary_embedding/mlu/rotary_embedding_cnnl.cpp
@@ -0,0 +1,100 @@
+#include "cnnl.h"
+#include "cnnl_extra.h"
+#include <vector>
+
+template<typename T>
+void RoPECnnlDevice(void *destination, void const *pos_ids, void const *sin_table, void const *cos_table, int *shape, int *strides, int total_seq_len, cnnlHandle_t &handle, cnrtQueue_t &queue){
+    int seq_len = shape[0];
+    int nhead = shape[1];
+    int dim = shape[2];
+
+    cnnlRotaryEmbeddingDescriptor_t ropeDesc;
+
+    cnnlCreateRotaryEmbeddingDescriptor(&ropeDesc);
+    cnnlSetRotaryEmbeddingDescriptor_v2(ropeDesc, false, true,
+                                        false, false, CNNL_SEQDATA_TNBC);
+
+    cnnlTensorDescriptor_t inDesc, posDesc, sinFullDesc, sinSelectedDesc, sinSelectedFP16Desc;
+    cnnlCreateTensorDescriptor(&inDesc);
+    cnnlCreateTensorDescriptor(&posDesc);
+    cnnlCreateTensorDescriptor(&sinFullDesc);
+    cnnlCreateTensorDescriptor(&sinSelectedDesc);
+    cnnlCreateTensorDescriptor(&sinSelectedFP16Desc);
+
+    int inShape[4] = {seq_len, 1, nhead, dim};
+    int inStrides[4] = {strides[0], strides[0], strides[1], strides[2]};
+        
+    int sinFullShape[2] = {total_seq_len, dim};
+    int sinSelectedShape[2] = {seq_len, dim};
+
+    cnnlDataType_t dataType;
+    if(sizeof(T) == 2){
+        dataType = CNNL_DTYPE_HALF;
+    }
+    else if(sizeof(T) == 4){
+        dataType = CNNL_DTYPE_FLOAT;
+    }
+    cnnlSetTensorDescriptorEx(inDesc, CNNL_LAYOUT_ARRAY, dataType, 4, inShape, inStrides);
+    cnnlSetTensorDescriptor(posDesc, CNNL_LAYOUT_ARRAY, CNNL_DTYPE_INT64, 1, &seq_len);
+    cnnlSetTensorDescriptor(sinFullDesc, CNNL_LAYOUT_ARRAY, CNNL_DTYPE_FLOAT, 2, sinFullShape);
+    cnnlSetTensorDescriptor(sinSelectedDesc, CNNL_LAYOUT_ARRAY, CNNL_DTYPE_FLOAT, 2, sinSelectedShape);
+    cnnlSetTensorDescriptor(sinSelectedFP16Desc, CNNL_LAYOUT_ARRAY, CNNL_DTYPE_HALF, 2, sinSelectedShape);
+
+    size_t size = cnnlGetTensorElementNum(sinSelectedDesc) * sizeof(float);
+    void *sinSelectedTable;
+    cnrtMalloc(&sinSelectedTable, size);
+    void *cosSelectedTable;
+    cnrtMalloc(&cosSelectedTable, size);
+
+    cnnlBatchGatherV2_v2(handle, 0, 0, 1,
+                        sinFullDesc, sin_table,
+                        posDesc, pos_ids,
+                        sinSelectedDesc, sinSelectedTable);
+    cnnlCastDataType(handle, sinSelectedDesc, sinSelectedTable,
+                    CNNL_CAST_FLOAT_TO_HALF, 
+                    sinSelectedFP16Desc, sinSelectedTable);
+    cnnlBatchGatherV2_v2(handle, 0, 0, 1,
+                        sinFullDesc, cos_table,
+                        posDesc, pos_ids,
+                        sinSelectedDesc, cosSelectedTable);
+    cnnlCastDataType(handle, sinSelectedDesc, cosSelectedTable,
+                    CNNL_CAST_FLOAT_TO_HALF,
+                    sinSelectedFP16Desc, cosSelectedTable);
+
+    // Do RoPE
+    cnnlRotaryEmbedding_v2(handle, ropeDesc,
+                        inDesc, destination, nullptr, nullptr,
+                        sinSelectedFP16Desc, cosSelectedTable,
+                        sinSelectedFP16Desc, sinSelectedTable,
+                        nullptr, nullptr, nullptr, nullptr, nullptr, 0,
+                        inDesc, destination, nullptr, nullptr);
+    CNRT_CHECK(cnrtQueueSync(queue));
+
+    cnrtFree(sinSelectedTable);
+    cnrtFree(cosSelectedTable);
+    cnnlDestroyRotaryEmbeddingDescriptor(ropeDesc);
+    cnnlDestroyTensorDescriptor(inDesc);
+    cnnlDestroyTensorDescriptor(posDesc);
+    cnnlDestroyTensorDescriptor(sinFullDesc);
+    cnnlDestroyTensorDescriptor(sinSelectedDesc);
+}
+template<typename T>
+void RoPECnnl(void *destination, void const *pos_ids, void const *sin_table, void const *cos_table, int *shape, int *strides, int total_seq_len)
+{
+    CNRT_CHECK(cnrtSetDevice(0));
+    cnnlHandle_t handle;
+    cnnlCreate(&handle);
+    cnrtQueue_t queue;
+    CNRT_CHECK(cnrtQueueCreate(&queue));
+    cnnlSetQueue(handle, queue); // 将队列绑定到 handle 中, 此接口也可用来更改句柄中的队列。
+
+    RoPECnnlDevice<T>(destination, pos_ids, sin_table, cos_table, shape, strides, total_seq_len, handle, queue);
+    
+    cnnlDestroy(handle);
+    CNRT_CHECK(cnrtQueueDestroy(queue));
+
+    
+}
+extern "C" void RoPE_cnnl_f16(void *destination, void const *pos_ids, void const *sin_table, void const *cos_table, int *shape, int *strides, int total_seq_len){
+    RoPECnnl<uint16_t>(destination, pos_ids, sin_table, cos_table, shape, strides, total_seq_len);
+}
---
+++ test/rotary_embedding.py
@@ -0,0 +1,150 @@
+import torch
+import ctypes
+import torch.nn as nn
+from functools import partial
+import argparse
+
+import performance
+# 添加上一层目录到模块搜索路径
+import sys
+import os
+
+lib_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), '.././build/lib/libmy_library.so')
+lib = ctypes.CDLL(lib_path)
+
+def reshape_for_broadcast(freqs_cis: torch.Tensor, x: torch.Tensor):
+    ndim = x.ndim
+    assert 0 <= 1 < ndim
+    assert freqs_cis.shape == (x.shape[0], x.shape[-1])
+    shape = [d if i == 0 or i == ndim - 1 else 1 for i, d in enumerate(x.shape)]
+    return freqs_cis.view(*shape)
+
+
+def rotary_embedding(t, pos, theta, torch_device):
+    dh = t.shape[2]
+    freqs = (1.0 / (theta ** (torch.arange(0, dh, 2)[: (dh // 2)].float() / dh))).to(
+        torch_device
+    )
+    
+    freqs = torch.outer(pos, freqs)#寒武纪不支持这种运算，如果在寒武纪机器测试，只能提前把数据移动到cpu
+    freqs_cis = torch.polar(torch.ones_like(freqs), freqs)
+    
+    t_ = torch.view_as_complex(t.reshape(*t.shape[:-1], -1, 2))
+    freqs_cis = reshape_for_broadcast(freqs_cis, t_)
+    t_out = torch.view_as_real(t_ * freqs_cis).flatten(2).to(t.dtype)
+    return t_out
+
+def sin_cos_table(max_seq_len, dim, torch_device, theta):
+    pos = torch.arange(
+        0, max_seq_len, dtype=torch.float32, device=torch.device(torch_device)
+    )
+    freqs = (1.0 / (theta ** (torch.arange(0, dim, 2)[: (dim // 2)].float() / dim))).to(
+        torch_device
+    )
+    # (a0, a1, a2) -> (a0, a0, a1, a1, a2, a2)
+    freqs = torch.repeat_interleave(freqs, repeats=2)
+    angles = torch.outer(pos, freqs)
+    return torch.sin(angles), torch.cos(angles)
+def test(test_shape, torch_device, test_dtype=torch.float16):
+    print(
+        f"Testing Rotary Positional Embedding on {torch_device} with shape:{test_shape} and dtype:{test_dtype}"
+    )
+    ndim = len(test_shape)
+    t = torch.rand(test_shape, dtype=test_dtype)
+    
+    pos = torch.arange(0, t.shape[0])
+    theta = 1e4
+    
+    pos = pos.to(torch.int64)
+    pos = pos.to(torch_device)
+    t = t.to(torch_device)
+
+    
+    # 2x table length for test
+    sin_table, cos_table = sin_cos_table(t.shape[0] * 2, t.shape[2], t.device, theta)
+
+    t_ptr = ctypes.cast(t.data_ptr(), ctypes.POINTER(ctypes.c_void_p))
+    pos_ptr = ctypes.cast(pos.data_ptr(), ctypes.POINTER(ctypes.c_void_p))
+    sin_ptr = ctypes.cast(sin_table.data_ptr(), ctypes.POINTER(ctypes.c_void_p))
+    cos_ptr = ctypes.cast(cos_table.data_ptr(), ctypes.POINTER(ctypes.c_void_p))
+    stride_0 = t.stride()[0]
+    stride_1 = t.stride()[1]
+    nt = test_shape[0]
+    nh = test_shape[1]
+    dimsize = test_shape[2]
+
+    import numpy as np
+    np_array = np.array(test_shape, dtype=np.int32)
+    shape = np_array.ctypes.data_as(ctypes.POINTER(ctypes.c_int))
+    strides = (ctypes.c_int * ndim)(*(t.stride()))
+    total_seq_len = sin_table.shape[0]
+
+    if test_dtype == torch.float16:
+        
+        if device == "mlu":
+            torch_RoPE_time = performance.BangProfile((rotary_embedding, (t.to("cpu"), pos.to("cpu"), theta, "cpu")))  
+            
+            lib.RoPE_cnnl_f16.argtypes = [
+                ctypes.POINTER(ctypes.c_void_p),
+                ctypes.POINTER(ctypes.c_void_p),
+                ctypes.POINTER(ctypes.c_void_p),
+                ctypes.POINTER(ctypes.c_void_p),
+                ctypes.POINTER(ctypes.c_int),
+                ctypes.POINTER(ctypes.c_int),
+                ctypes.c_int
+            ]
+            custom_RoPE_time = \
+            performance.BangProfile((lib.RoPE_cnnl_f16, (t_ptr, pos_ptr, sin_ptr, cos_ptr, shape, strides, total_seq_len)))
+            '''
+            lib.RoPE_bang_f16.argtypes = [
+                ctypes.POINTER(ctypes.c_void_p),
+                ctypes.POINTER(ctypes.c_void_p),
+                ctypes.POINTER(ctypes.c_void_p),
+                ctypes.POINTER(ctypes.c_void_p),
+                ctypes.c_int,
+                ctypes.c_int,
+                ctypes.c_int,
+                ctypes.c_int,
+                ctypes.c_int
+            ]
+            custom_RoPE_time = \
+            performance.BangProfile((lib.RoPE_bang_f16, (t_ptr, pos_ptr, sin_ptr, cos_ptr, stride_0, stride_1, nt, nh, dimsize)))
+            '''
+            
+    performance.logBenchmark(torch_RoPE_time, custom_RoPE_time)
+    tmpa = rotary_embedding(t.to("cpu"), pos.to("cpu"), theta, "cpu").detach().numpy().flatten()
+    
+    tmpb = t.to('cpu').detach().numpy().flatten()
+    
+    atol = max(abs(tmpa - tmpb))
+
+    rtol = atol / max(abs(tmpb) + 1e-8)
+
+
+    print("absolute error:%.4e"%(atol))
+    print("relative error:%.4e"%(rtol))
+    
+# 解析命令行参数
+parser = argparse.ArgumentParser(description="Test rotary_embedding on different devices.")
+parser.add_argument('--device', choices=['cpu', 'cuda', 'mlu'], required=True, help="Device to run the tests on.")
+args = parser.parse_args()    
+
+test_cases = [
+        ((1, 32, 128), "mlu", torch.float16),
+        ((1, 32, 64), "mlu", torch.float16),
+        
+        ((4, 1, 32), "mlu", torch.float16),
+        ((1, 32, 128), "mlu", torch.float16),
+        
+        ((3, 32, 128), (8000, 200, 1), torch.float16),
+    ]
+filtered_test_cases = [
+    (test_shape, device, test_dtype)
+    for test_shape, device, test_dtype in test_cases
+    if device == args.device
+]
+if args.device == 'mlu':
+    import torch_mlu
+# 执行过滤后的测试用例
+for test_shape, device, test_dtype in filtered_test_cases:
+    test(test_shape, device, test_dtype)
